6a75cc56f756b67805f004d605efb8743b16cf51
David Billskog <billskog@gmail.com>
2012-08-10 11:15:07 +0200

    Added workflowy to dotjs and updated di.se script

--- dotjs/di.se.js
+++ dotjs/di.se.js
@@ -1,3 +1,3 @@
 g = document.getElementsByTagName('frameset')[0];
-g.rows = "0,*";
+g.rows = "0,0,*";
 
---
+++ dotjs/workflowy.com.js
@@ -0,0 +1,4 @@
+$(".editor > textarea").bind("keydown","alt+up",function() { $(this).getProject().getPreviousSibling().children(".name").moveCursorToBeginning(); return false;});
+$(".editor > textarea").bind("keydown","alt+down",function() { $(this).getProject().getNextSibling().children(".name").moveCursorToBeginning(); return false;});
+$("#dropdownMessages").hide();
+$('#share_buttons').hide();$('#share_buttons').hide();
