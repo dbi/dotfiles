2105bad1d3bd5ebbe3fd254364432eddc7540b16
David Billskog <billskog@gmail.com>
2012-08-18 19:27:43 +0200

    Updated di.se script

--- dotjs/di.se.js
+++ dotjs/di.se.js
@@ -1,3 +1,11 @@
+// make sure jQuery is included
+
 g = document.getElementsByTagName('frameset')[0];
 g.rows = "0,0,*";
 
+jQuery(function() {
+  setTimeout(function() {
+    jQuery(".wrapperfixed", parent.frames[2].document).hide();
+    jQuery("#hud", parent.frames[2].document).hide();
+  }, 1500);
+});
